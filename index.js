269a66ff347014ba37907251c0653e6a09f428bf
Milica <magiflowerchild@gmial.com>
2022-03-11 00:10:02 +0100

    added asynchronous functionality

--- index.js
+++ index.js
@@ -1,46 +1,102 @@
-class PostOffice {
+class Queue {
   constructor() {
-    if (this.constructor === PostOffice) {
-      throw new Error("This is an abstract class");
+    this.list = [];
+  }
+  enqueue(letter) {
+    return this.list.push(letter);
+  }
+  dequeue() {
+    return this.list.shift();
+  }
+  front() {
+    return this.list[0];
+  }
+  isEmpty() {
+    if (this.list.length === 0) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
-class Customer extends PostOffice {
+class PostOffice {
+  constructor() {
+    this.listOfLetters = new Queue();
+    setInterval(async () => {
+      console.log("sending..");
+      if (this.listOfLetters.isEmpty()) {
+        return console.log("All letters have been sent");
+      }
+      try {
+        let data = await this.sendLetter(this.listOfLetters.front());
+        if (data.receiver !== data.sender) {
+          data.receiver.acceptLetter(data);
+        } else {
+          throw new Error("attempted auto send");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      this.listOfLetters.dequeue();
+    }, 10000);
+  }
+  addLetter(letter) {
+    this.listOfLetters.enqueue(letter);
+  }
+  sendLetter(letter) {
+    return new Promise((resolve, reject) => {
+      if (Math.random() > 0.1) {
+        setTimeout(() => {
+          resolve(letter);
+        }, 3000);
+      } else {
+        reject(Error("We lost the letter, Oopsie!"));
+      }
+    });
+  }
+}
+
+class Customer {
   constructor(firstName, lastName) {
-    super();
     this.firstName = firstName;
     this.lastName = lastName;
   }
   acceptLetter(letter) {
-    if (
-      this.firstName === letter.receiver.firstName &&
-      this.lastName === letter.receiver.lastName
-    ) {
-      return console.log(
-        "This letter was sent by " +
-          letter.sender.firstName +
-          " " +
-          letter.sender.lastName +
-          "and its contents are:\n" +
-          letter.content
-      );
-    } else {
-      console.log("You dont have any letters from this sender");
-    }
+    return console.log(
+      "This letter was sent by " +
+        letter.sender.firstName +
+        " " +
+        letter.sender.lastName +
+        " and its contents are:\n" +
+        letter.content
+    );
   }
 }
-class Letter extends PostOffice {
-  constructor(sender, receiver, content) {
-    super();
-    this.sender = sender;
-    this.receiver = receiver;
+class Letter {
+  constructor(content) {
+    this.sender = new Customer();
+    this.receiver = new Customer();
     this.content = content;
   }
 }
 
-let person1 = new Customer("Marko Markovic", "");
+let PostaSrbije = new PostOffice();
+let person1 = new Customer("Marko", "Markovic");
 let person2 = new Customer("Ivan", "Ivanovic");
 
-let letter1 = new Letter(person1, person2, "Hello there!");
-console.log(letter1);
-person2.acceptLetter(letter1);
+let letter1 = new Letter("Hello there!");
+let letter2 = new Letter("Hello again!");
+let letter3 = new Letter("Mailing this to myself");
+letter1.sender = person1;
+letter1.receiver = person2;
+letter2.sender = person2;
+letter2.receiver = person1;
+letter3.sender = person1;
+letter3.receiver = person1;
+// console.log(letter1);
+// console.log(letter2);
+
+// person2.acceptLetter(letter1);
+PostaSrbije.addLetter(letter1);
+PostaSrbije.addLetter(letter2);
+PostaSrbije.addLetter(letter3);
